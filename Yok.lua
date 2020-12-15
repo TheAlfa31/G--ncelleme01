1daa52b507efc80a0c0ba403edf567fd669ad1f1
TheAlfa31 <70370022+TheAlfa31@users.noreply.github.com>
2020-12-15 23:43:56 +0300

    Yok.lua
    T

--- Yok.lua
+++ Yok.lua
@@ -13,13 +13,12 @@ end
 gg.alert("🔰𝐕𝐢𝐩 𝐒𝐜𝐫𝐢𝐩𝐭'𝐞 𝐇𝐨ş 𝐠𝐞𝐥𝐝𝐢𝐧🔰","Devam")
 gg.alert("🛡️𝐋𝐨𝐛𝐢𝐝𝐞 𝐀𝐧𝐭𝐢-𝐁𝐚𝐧 𝐀ç𝐦𝐚𝐲ı 𝐔𝐧𝐮𝐭𝐦𝐚🛡️","Tamamdır")
 gg.toast("💎Iyi Oyunlar💎")
-print("\n𝐅𝐨𝐫𝐰𝐚𝐫𝐝 𝐇𝐚𝐜𝐤 𝐓𝐞𝐚𝐦")
-cvd="\n██╗░░░██╗██╗██████╗░\n██║░░░██║██║██╔══██╗\n╚██╗░██╔╝██║██████╔╝\n░╚████╔╝░██║██╔═══╝░\n░░╚██╔╝░░██║██║░░░░░\n░░░╚═╝░░░╚═╝╚═╝░░░░░\n                                                   2.5"
-gg.alert(cvd)
+
 status="✅Hile Güvenli✅\nToplam Banlanan Sayısı:0\nİncelemeye Takılanlar(Tahmin):0\nCheat System Kurbanları:0"
 
-on = " [✓]"
-off = " [×]"
+
+on = " [✅]"
+off = " [❌]"
 
 
 
@@ -30,23 +29,23 @@ anten_checker = off
 less_checker = off
 elli_kafadanchecker = off
 elliaimbot_checker = off
-
+magic_checker = off
+brutal_kafadanchecker = off
+norecoil_checker = off
 
 function HOME()
 SN = gg.choice({
    "🛡️Anti-Ban Menü[Lobi]🛡️",
-   "💎Hileler[Oyun+Lobi]💎",
-   "🔥Brutal Menü[YOK]🔥",
-   "🌈Diğer Hileler[YOK]🌈",
+   "💎Safe Menü[Lobi+Oyun]💎",
+   "😈Brutal Menü[Lobi+Oyun]😈",
    "⛔Çıkış⛔",
-	 }, nil, status)
+	 }, nil, '✨Zero Hack Vip Menü✨')
 	
 if SN == nil then else
 if SN==1 then F1() end
 if SN==2 then F2() end
 if SN==3 then F3() end
-if SN==4 then F4() end
-if SN==5 then Exit() end
+if SN==4 then Exit() end
 end 
   PUBGMH = -1
 end
@@ -62,96 +61,8 @@ saf=gg.choice({
 if saf==1 then
 if antiban_checkerer == off then
  antiban_checkerer = on
-gg.alert("🛡️𝐅𝐨𝐫𝐰𝐚𝐫𝐝 𝐇𝐚𝐜𝐤 𝐓𝐞𝐚𝐦 𝐀𝐧𝐭𝐢 𝐁𝐚𝐧🛡️")
-os.remove("src/main/java/com/google/errorprone/annotations")
-os.remove("src/main/java/com/google/errorprone/annotations")
-os.remove("src/main/java/com/google/errorprone/annotations/concurrent")
-os.remove("third_party.java_src.error_prone.project.annotations.Google_internal")
-os.remove("src/main/java/com/google/errorprone/annotations")
-os.remove("src/main/java/com/google/errorprone/annotations")
-os.remove("src/main/java/com/google/errorprone/annotations/concurrent")
-os.remove("third_party.java_src.error_prone.project.annotations.Google_internal")
-gg.processResume()
-gg.processResume()
-gg.searchNumber("65535", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
-gg.processResume()
-gg.processResume()
-gg.searchNumber("262143", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
-gg.processResume()
-gg.refineNumber("262144", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
-gg.searchNumber("262144", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
-gg.processResume()
-gg.processResume()
-gg.clearResults()
-gg.searchNumber("131073", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
-gg.processResume()
-gg.processResume()
-gg.clearResults()
-gg.searchNumber("135168", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
-gg.processResume()
-gg.clearResults()
-gg.searchNumber("196608", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
-gg.processResume()
-gg.clearResults()
-gg.searchNumber("192512", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
-gg.processResume()
-gg.refineNumber("4096", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
-gg.searchNumber("4096", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
-gg.processResume()
-os.remove("/storage/emulated/0/.backups")
-os.remove("/storage/emulated/0/.chaozhuo.gameassistant")
-os.remove("/storage/emulated/0/.DataStorage")
-os.remove("/storage/emulated/0/.mcs")
-os.remove("/storage/emulated/0/.SAU")
-os.remove("/storage/emulated/0/Android/data/com.tencent.iglite/files/.system_android_l2")
-os.remove("/storage/emulated/0/Android/data/com.tencent.iglite/files/ca-bundle.pem")
-os.remove("/storage/emulated/0/Android/data/com.tencent.iglite/files/ProgramBinaryCache")
-os.remove("/storage/emulated/0/Android/data/com.tencent.iglite/files/.fff")
-os.remove("/storage/emulated/0/Android/data/com.tencent.iglite/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Config/Android/IGH5CachePluginConfig.ini")
-os.remove("/storage/emulated/0/Android/data/com.tencent.iglite/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Config/Android/CustomDeviceList.ini")
-os.remove("/storage/emulated/0/Android/data/com.tencent.iglite/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Config/Android/DeviceProfiles.ini")
-os.remove("/storage/emulated/0/Android/data/com.tencent.iglite/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Config/Android/IMSDKConfig.ini")
-os.remove("/storage/emulated/0/Android/data/com.tencent.iglite/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Config/Android/UAE.ini")
-os.remove("/storage/emulated/0/Android/data/com.tencent.iglite/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/ImageDownload/")
-os.remove("/storage/emulated/0/Android/data/com.tencent.iglite/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Logs/")
-os.remove("/storage/emulated/0/Android/data/com.tencent.iglite/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/PufferTmpDir/")
-os.remove("/storage/emulated/0/Android/data/com.tencent.iglite/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/SaveGames/Activity")
-os.remove("/storage/emulated/0/Android/data/com.tencent.iglite/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/SaveGames/Advertise")
-os.remove("/storage/emulated/0/Android/data/com.tencent.iglite/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/SaveGames/GEM")
-os.remove("/storage/emulated/0/Android/data/com.tencent.iglite/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/SaveGames/PersonSpace")
-os.remove("/storage/emulated/0/Android/data/com.tencent.iglite/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/SaveGames/Pet")
-os.remove("/storage/emulated/0/Android/data/com.tencent.iglite/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/SaveGames/RoleInfo")
-os.remove("/storage/emulated/0/Android/data/com.tencent.iglite/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/SaveGames/playerprefs.json")
-os.remove("/storage/emulated/0/Android/data/com.tencent.iglite/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/SaveGames/playerprefs.sav")
-os.remove("/storage/emulated/0/Android/data/com.tencent.iglite/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/UpdateInfo/")
-os.remove("/storage/emulated/0/Android/data/com.tencent.iglite/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/StatEventReportedFlag")
-os.remove("/storage/emulated/0/Android/data/com.tencent.iglite/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Paks/PufferFileList.json")
-os.remove("/data/user/0/com.tencent.iglite.qqig/app_opt")
-os.remove("/data/user/0/com.tencent.iglite.qqig/cache")
-os.remove("/data/user/0/com.tencent.iglite.qqig/code_cache")
-os.remove("/data/user/0/com.tencent.iglite.qqig/files")
-os.remove("/data/user/0/com.tencent.iglite.qqig/shared_prefs")
-os.remove("/data/user/0/com.tencent.iglite.qqig/com.tencent.ig/data/app/system/device-info.ini")
-os.remove("/data/user/0/com.tencent.iglite.qqig/com.tencent.ig/data/app/system/job-list.ini")
-os.remove("/data/user/0/com.tencent.iglite.qqig/com.tencent.ig/data/app/system/uid-list.ini")
-os.remove("/data/user/0/com.tencent.iglite.qqig/com.tencent.ig/data/app/system/virtual-loc.ini")
-os.remove("/data/user/0/com.tencent.iglite.qqig/com.tencent.ig/data/data/0/com.tencent.iglite/app_bugly")
-os.remove("/data/user/0/com.tencent.iglite.qqig/com.tencent.ig/data/data/0/com.tencent.iglite/app_crashrecord")
-os.remove("/data/user/0/com.tencent.iglite.qqig/com.tencent.ig/data/data/0/com.tencent.iglite/cache")
-os.remove("/data/user/0/com.tencent.iglite.qqig/com.tencent.ig/data/data/0/com.tencent.iglite/code_cache")
-os.remove("/data/user/0/com.tencent.iglite.qqig/com.tencent.ig/data/data/0/com.tencent.iglite/files/local_crash_lock")
-os.remove("/data/user/0/com.tencent.iglite.qqig/com.tencent.ig/data/data/0/com.tencent.iglite/no_backup")
-os.remove("/data/user/0/com.tencent.iglite.qqig/com.tencent.ig/data/data/0/wifiMacAddress")
-os.remove("/storage/emulated/0/MidasOverse")
-os.remove("/storage/emulated/0/Android/data/com.pubg.krmobile/files/tbslog")
-os.remove("/storage/emulated/0/Android/data/com.pubg.krmobile/files/login-identifier.txt")
-os.remove("/storage/emulated/0/Android/data/com.pubg.krmobile/files/cacheFile.txt")
-os.remove("/storage/emulated/0/Android/data/com.pubg.krmobile/files/vmpcloudconfig.json")
-os.remove("/storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Logs")
-os.remove("/storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/GameErrorNoRecords")
-os.remove("/storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Config/Android/AntiCheat.ini")
-os.remove("/storage/emulated/0/Android/data/com.pubg.krmobile/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Config/Android/IGH5CachePluginConfig.ini")
-os.remove("/storage/emulated/0/Android/data/com.pubg.krmobile/cache")
+gg.alert("🛡️𝐀𝐧𝐭𝐢 𝐁𝐚𝐧🛡️")
+HOME()
 else
 antiban_checkerer = on
 gg.alert("Anti Banı Bikere Çalıştırman Yeter Kardeşim❤️")
@@ -159,27 +70,23 @@ end
 end
 
 if saf==2 then
-if antibank_checkerer == off then
-antibank_checkerer = on
-gg.alert("🔰Bu Bölüm üzerinde Hala Çalışılıyor...🔰")
-else
-antibank_checkerer = on
-gg.alert("Bikere Çalıştırman Yeter Kardeşim❤️")
-end
+gg.alert('Bakım')
+HOME()
+----if antibank_checkerer == off then
+----antibank_checkerer = on
+-----gg.alert("🔰Bu Bölüm üzerinde Hala Çalışılıyor...🔰")
+----else
+----antibank_checkerer = on
+-----gg.alert("Bikere Çalıştırman Yeter Kardeşim❤️")
+----end
 end
 
 if saf==3 then
-if ucparti_checkerer == off then
-ucparti_checkerer = on
-gg.alert("3. Parti Önlendi")
-else
-ucparti_checkerer = on
-gg.alert("Gerek Yok")
-end
+gg.alert('Üzerinde Çalışılıyor...')
+HOME()
 end
 
 if saf==4 then
-gg.toast("Yok")
 HOME()
 end
 end
@@ -189,12 +96,10 @@ function F2()
 sf=gg.choice({
 	"📡Anten [Lobi+Oyun]📡"..anten_checker,
 	"🔫Less Recoil [Oyun]🔫"..less_checker,
-	"🤯Brutal Kafadan Vurma[Lobi+Oyun]🤯"..elli_kafadanchecker,
-	"➕Küçük Artı➕[Oyun]",
-	"🔫AimBot 360⁰ Yeni[Oyun]🔫"..elliaimbot_checker,
+	"💀%50 Kafadan Vurma[Oyun]💀",
 	"🎥Ipad Mod[Oyun]🎥",
 	"🔙Geri🔙"
-	},nil,"🛡️Hile Menü🛡️\n👤ByAlfa👤\n👤Byman👤")
+	},nil,"🛡️Hile Menü🛡️")
 	
 if sf==1 then
 if anten_checker == off then
@@ -213,7 +118,7 @@ gg.searchNumber("32769", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
 anteniki=gg.getResults(10)
 gg.editAll("4", gg.TYPE_DWORD)
 gg.clearResults()
-gg.toast("Anten✓")
+gg.toast("Anten")
 else
 if anten_checker == on then
 anten_checker = off
@@ -231,24 +136,12 @@ gg.clearResults()
   gg.setRanges(gg.REGION_ANONYMOUS)
   gg.searchNumber("1,348,759,109;1,953,067,887;1,634,692,166;1,920,287,604:512", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
   gg.searchNumber("1,634,692,166", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
-  less=gg.getResults(10)
+  lessrecoil = gg.getResults(10)
   gg.editAll("0", gg.TYPE_DWORD)
   gg.clearResults()
-  gg.clearResults()
-  gg.setRanges(gg.REGION_ANONYMOUS)
-  gg.setVisible(false)
-  gg.searchNumber("0.2~0.3;53;30;1::", gg.TYPE_FLOAT)
-  gg.setVisible(false)
-  gg.searchNumber("0.2~0.3;1::", gg.TYPE_FLOAT)
-  lessiki=gg.getResults(200)
-  gg.editAll("1.4012985e-45", gg.TYPE_FLOAT)
-  gg.clearResults()
-  gg.toast("No Recoil Aktif")
+gg.toast("Less Recoil")
 else
-if less_checker == on then
-less_checker = off
-gg.setValues(less)
-gg.setValues(lessiki)
+gg.setValues(lessrecoil)
 gg.toast("Less Recoil Devre Dışı")
 end
 end
@@ -257,6 +150,112 @@ end
 if sf==3 then
 if elli_kafadanchecker == off then
 elli_kafadanchecker = on
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("9.201618;30.5;25", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("25;30.5", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+kafabir=gg.getResults(10)
+gg.editAll("150", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("4740038608910024704", gg.TYPE_QWORD, false, gg.SIGN_EQUAL, 0, -1)
+kafaiki=gg.getResults(100)
+gg.editAll("5306590012668116992", gg.TYPE_QWORD)
+gg.setVisible(false)
+gg.toast("Aktif")
+gg.clearResults()
+else
+if elli_kafadanchecker == on then
+elli_kafadanchecker = off
+gg.setValues(kafabir)
+gg.setValues(kafaiki)
+gg.toast("Kapalı")
+end
+end
+end
+
+if sf==4 then
+gg.clearResults()
+gg.setRanges(16384)
+gg.searchNumber("360.0;0.00100000005;-9.38575022e22", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("360.0;0.00100000005", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(2)
+gg.editAll("-1.4235595e-27;254.70928955078", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast("İpad Modu Aktif")
+end
+
+if sf==5 then
+HOME()
+end
+
+function F3()
+br=gg.choice({
+	"📡Anten [Lobi+Oyun]📡"..anten_checker,
+	"🔥Magic Brutal[Oyun]🔥"..magic_checker,
+    "🤯Brutal Kafadan Vurma[Lobi+Oyun]🤯"..brutal_kafadanchecker,
+	"🔫AimBot 360⁰ Yeni[Oyun]🔫"..elliaimbot_checker,
+	"🔫Sıfır Sekme[Lobi]🔫"..norecoil_checker,
+	"🔙Geri🔙" })
+	
+if br==1 then
+if anten_checker == off then
+anten_checker = on
+gg.clearResults()
+gg.setRanges(gg.REGION_ANONYMOUS)
+gg.searchNumber("88.50576019287F;87.27782440186F;-100.91194152832F;1F::13", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("88.50576019287F;87.27782440186F;1F", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+antenbir=gg.getResults(100)
+gg.editAll("1.96875;1.96875;999;1.96875;1.96875;999", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast("Lutfen Bekleyiniz")
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("32768;-2134900726;32769;-2134900725;32770;-2134900724::", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("32769", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+anteniki=gg.getResults(10)
+gg.editAll("4", gg.TYPE_DWORD)
+gg.clearResults()
+gg.toast("Anten")
+else
+if anten_checker == on then
+anten_checker = off
+gg.setValues(antenbir)
+gg.setValues(anteniki)
+gg.toast("Anten Kapalı")
+end
+end
+end
+
+if br==2 then
+if magic_checker == off then
+magic_checker = on
+gg.clearResults()
+gg.setRanges(32)
+gg.searchNumber("1F;-8.6457681e12F;15F;28F;16F;26F;8F;18F:512", 16, false, 536870912, 0, -1)
+gg.searchNumber("15.0F;28.4F;16.0F;26.0F;8.4F;18.4F:512", 16, false, 536870912, 0, -1)
+magic=gg.getResults(500)
+gg.editAll("95", 16)
+gg.clearResults()
+gg.setRanges(32)
+gg.searchNumber("9.20161819458.43;25;30.5", 16, false, 536870912, 0, -1)
+gg.searchNumber("25;30.5", 16, false, 536870912, 0, -1)
+magiciki=gg.getResults(100)
+gg.editAll("350", 16)
+gg.clearResults()
+gg.toast("Magic Bullet Brutal")
+else
+if magic_checker == on then
+magic_checker = off
+gg.setValues(magic)
+gg.setValues(magiciki)
+gg.toast("Magic Bullet Brutal Devre Dışı")
+end
+end
+end
+
+if br==3 then
+if brutal_kafadanchecker == off then
+brutal_kafadanchecker = on
 gg.setRanges(gg.REGION_ANONYMOUS)
 gg.searchNumber("9.201618;30.5;25", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
 gg.searchNumber("25;30.5", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
@@ -272,8 +271,8 @@ gg.setVisible(false)
 gg.toast("Aktif")
 gg.clearResults()
 else
-if elli_kafadanchecker == on then
-elli_kafadanchecker = off
+if brutal_kafadanchecker == on then
+brutal_kafadanchecker = off
 gg.setValues(kafabir)
 gg.setValues(kafaiki)
 gg.toast("Kapalı")
@@ -281,22 +280,7 @@ end
 end
 end
 
-if sf==4 then
-gg.setRanges(gg.REGION_CODE_APP)
-gg.setVisible(false)
-gg.setVisible(false)
-gg.searchNumber("-1.11445016E28", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
-gg.setVisible(false)
-gg.refineAddress("7B8", -1, gg.TYPE_FLOAT, gg.SIGN_EQUAL, 0, -1)
-gg.setVisible(false)
-gg.getResults(891)
-gg.setVisible(false)
-gg.editAll("130", gg.TYPE_FLOAT)
-gg.clearResults()
-gg.toast("Küçük Artı")
-end
-
-if sf==5 then
+if br==4 then
 if elliaimbot_checker == off then
 elliaimbot_checker = on
 gg.clearResults()
@@ -328,34 +312,33 @@ end
 end
 end
 
-if sf==6 then
+if br==5 then
+if norecoil_checker == off then
+norecoil_checker = on
 gg.clearResults()
-gg.setRanges(16384)
-gg.searchNumber("360.0;0.00100000005;-9.38575022e22", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
-gg.refineNumber("360.0;0.00100000005", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
-gg.getResults(2)
-gg.editAll("-1.4235595e-27;254.70928955078", gg.TYPE_FLOAT)
+gg.setRanges(gg.REGION_CODE_APP)
+gg.searchNumber("495,979,008;-300,938,736;-516,948,194:9", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("-300,938,736", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+norecoil = gg.getResults(551)
+gg.editAll("0", gg.TYPE_DWORD)
 gg.clearResults()
-gg.toast("İpad Modu Aktif")
+gg.toast("Sekmeme Aktif Edildi")
+else
+if norecoil_checker == on then
+norecoil_checker = off
+gg.setValues(norecoil)
+gg.toast("Sekmeme Kapatıldı")
+end
+end
 end
 
-if sf==7 then
-gg.toast("Geri")
+if br==6 then
 HOME()
 end
-end
-
-function F3()
-HOME()
-end
-
-function F4()
-gg.alert("⚠️Bakım⚠️")
-HOME()
 end
 
 function Exit()
-print("✅Boost İşlemi Tamamdır✅\n👤Coder:By:Alfa👤\n👤Coder:Byman👤\n🛡️S16 Vip V1🛡️")
+print("✅Boost İşlemi Tamamdır✅\n🛡️S16 Vip V1🛡️\n🔥Zero Hack🔥")
 gg.skipRestoreState()
   os.exit()
   gg.setVisible(true)
