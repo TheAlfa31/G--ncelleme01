e88178ba1da18a27fcba0da1eb2e4b4507c7dca0
TheAlfa31 <70370022+TheAlfa31@users.noreply.github.com>
2020-12-16 08:56:36 +0300

    Yok.lua
    T

--- Yok.lua
+++ Yok.lua
@@ -463,7 +463,7 @@ PS = gg.prompt({'✅Giriş Paneli✅'},{[1]=''},{[1]='text'})
 if not PS the﻿n os.exit() 
 end 
 if PS[1] == Password then 
-gg.toast('Key Doğrulandı'
+gg.toast('Key Doğrulandı')
 gg.alert("❤️Hoş Geldiniz Efendim❤️")
 else 
 gg.alert('Geçersiz Anahtar\n⚠️Admin Değilsin⚠️') os.exit() end
