78cebd59124ea6787380cae41871424fe4ba2674
TheAlfa31 <70370022+TheAlfa31@users.noreply.github.com>
2020-12-16 13:37:06 +0300

    Cumburlop.lua
    Zhhshshsjs

--- Cumburlop.lua
+++ Cumburlop.lua
@@ -589,6 +589,15 @@ end
 end
 
 function bedava()
+Password﻿ = 'SnLcYSbBBaTvqtkabKhiy'
+PS = gg.prompt({'✅Giriş Paneli✅'},{[1]=''},{[1]='text'})
+if not PS the﻿n os.exit() 
+end 
+if PS[1] == Password then 
+gg.toast('✅Key Doğrulandı✅')
+else 
+gg.alert('Geçersiz Anahtar\n⚠️Anahtar @zeroohack de Paylaşıldı⚠️') os.exit() end
+
 SnLcYSbBBaTvqtkabKhiy = '99999999'
 if os.date("%Y%m%d") > SnLcYSbBBaTvqtkabKhiy then
   hh = gg.alert("⛔Script Süreniz Dolmuştur⛔")
