6e86526ae0b46f90cf90abbf063c71cdb48c5fc8
TheAlfa31 <70370022+TheAlfa31@users.noreply.github.com>
2021-04-29 20:36:24 +0300

    Update onlinetaban.sh

--- onlinetaban.sh
+++ onlinetaban.sh
@@ -1,3 +1,7 @@
+# echo "bash <(curl -L https://raw.githubusercontent.com/TheAlfa31/G--ncelleme01/main/onlinetaban.sh)" > /storage/emulated/0/execute.sh
+# bash execute.sh
+
+echo '
 echo "
 ----------------------------------------
            [Versiyon 1.0]
@@ -8,4 +12,5 @@ echo "
 rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Logs
 echo "Loglar Silindi!\n\n"
 echo "Kapandı!"
-
+' > /storage/emulated/0/yereltaban.sh
+sh yereltaban.sh
