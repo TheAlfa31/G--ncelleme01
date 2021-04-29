3cd21212223b2783bfe02c41c83763799f9c011c
TheAlfa31 <70370022+TheAlfa31@users.noreply.github.com>
2021-04-29 20:10:23 +0300

    Create onlinetaban.sh

---
+++ onlinetaban.sh
@@ -0,0 +1,11 @@
+echo "
+----------------------------------------
+           [Versiyon 1.0]
+           [Dev: Tc Alfa]
+  [Çok Amaçlı Online Veri Tabanı]
+----------------------------------------
+"
+rm -rf /storage/emulated/0/Android/data/com.tencent.ig/files/UE4Game/ShadowTrackerExtra/ShadowTrackerExtra/Saved/Logs/*
+echo "Loglar Silindi!\n\n"
+echo "Kapandı!"
+
