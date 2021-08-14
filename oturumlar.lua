8a789ff079916021d3d8ae4ee85c09d2af16790d
TheAlfa31 <70370022+TheAlfa31@users.noreply.github.com>
2021-08-14 18:42:33 +0300

    Add files via upload

---
+++ oturumlar.lua
@@ -0,0 +1,17 @@
+VUNuTsSerEDbkRUFMaYG = '99999999'
+Password﻿ = 'VUNuTsSerEDbkRUFMaYG'
+PS = gg.prompt({'✅Giriş Paneli✅'},{[1]=''},{[1]='text'})
+if not PS the﻿n os.exit() 
+end 
+if PS[1] == Password then 
+gg.toast('Key Doğrulandı')
+gg.alert("❤️Hoş Geldiniz Efendim❤️")
+else 
+gg.alert('Geçersiz Anahtar\n⚠️Admin Değilsin⚠️') os.exit() end
+
+if os.date("%Y%m%d") > VUNuTsSerEDbkRUFMaYG then
+  hh = gg.alert("⛔ Key Süreniz Dolmuştur ⛔")
+  os.exit()
+end
+asd()
+end
